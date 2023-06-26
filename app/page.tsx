4bb582c76d081952da2148dd65f82f9b845393b3
Alejandro G. Lacasa <alejandro.lacasa@nexum.com>
2023-06-26 19:32:04 +0200

    Fixes for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,29 @@
 export default function Home() {
   return (
-    <main className="container px-4 md:px-16 lg:px-32 mx-auto flex justify-center flex-col text-center">
-      <p className="flex items-center justify-center text-7xl h-screen">
+    <main className="relative container px-8 md:px-16 lg:px-32 mx-auto flex justify-center flex-col text-center">
+      <div className="opacity-50 inset-0 background fixed w-screen h-screen z-[-1]" />
+      <p className="flex items-center justify-center text-5xl md:text-7xl h-screen">
         Hola Carlos y Jessi
       </p>
       <p className="flex items-center justify-center text-9xl h-screen">👇🏻</p>
-      <p className="flex items-center justify-center text-7xl h-screen">
+      <p className="flex items-center justify-center text-2xl md:text-5xl h-screen">
         El equipo de Informática 3E...
       </p>
       <p className="flex items-center justify-center text-9xl h-screen">👇🏻</p>
-      <p className="flex items-center justify-center text-7xl h-screen">
+      <p className="flex items-center justify-center text-2xl md:text-5xl h-screen">
         ...tiene un mensaje muy especial
       </p>
       <p className="flex items-center justify-center text-9xl h-screen">👇🏻</p>
-      <p className="flex items-center justify-center text-5xl h-screen">
+      <p className="flex items-center justify-center text-2xl md:text-5xl h-screen">
         Seguid...
       </p>
       <p className="flex items-center justify-center text-9xl h-screen">👇🏻</p>
-      <p className="flex items-center justify-center text-5xl h-screen">
+      <p className="flex items-center justify-center text-2xl md:text-5xl h-screen">
         Más...
       </p>
       <p className="flex items-center justify-center text-9xl h-screen">👇🏻</p>
       <div className="flex flex-col items-center justify-center h-screen">
-        <p className="text-5xl pb-8">Denle al play y disfruten</p>
+        <p className="text-2xl md:text-5xl pb-8">Denle al play y disfruten</p>
         <video
           className="max-w-[720px] w-full h-auto"
           autoPlay={false}
